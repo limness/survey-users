8d3f67a76ca841f9fe7f08d586080250aabf4ef6
Andrew Lim <andrey.pwn@gmail.com>
2021-10-22 15:12:19 +0300

    change settings

--- survey/settings.py
+++ survey/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "localhost",
-        "PORT": 5432,
-        "CONN_MAX_AGE": 600,
+        # "PORT": 5432,
+        # "CONN_MAX_AGE": 600,
     }
 }
 # DATABASES = {
